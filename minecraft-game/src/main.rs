cf35e3b3f20c59c3c34f3f6047430726c2ee5761
SDK100 <87295447+SDK100@users.noreply.github.com>
2026-06-25 17:02:47 -0400

    added game

--- minecraft-game/src/main.rs
+++ minecraft-game/src/main.rs
@@ -1,8 +1,21 @@
+use std::{fmt::format, os, time::SystemTime};
+
+use chrono::{DateTime, Utc};
+
+
 fn main(){
 
+    let mut now = SystemTime::now();
+    let translated:DateTime<Utc> = now.into();
 
     'test: loop {
-        print!("hi");
+        print!("{}", now);
         break 'test;
     }
+}
+
+
+fn quit_function(){
+
+
 }
